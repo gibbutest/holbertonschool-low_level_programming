17663c01e56d514d9a2d84cbb99c5c0a8d479520
Ian <Gibbu_@hotmail.com>
2025-02-17 13:27:24 +1100

    stuff" !q
    q
    exit
    !q
    as/dl
    kfnqas
    fhkfgsm
    edf l;kqsnm
    dfl;k m
    f
     ml;qsd
    f

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0) {
+
+	if (n < 0)
 		printf("%d is negative", n);
-	}
 	else if (n > 0)
-	{
 		printf("%d is positive", n);
-	}
 	else
-	{
 		printf("%d is zero", n);
-	}
+
 	return (0);
 }
