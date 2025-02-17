a3dca1ae5dec782d58c05bc005d5fe4ae9244714
Ian <Gibbu_@hotmail.com>
2025-02-17 13:42:57 +1100

    stuff

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main
+ *
+ * Return: return
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	
+	putchar('\n');
+
+	return (0);
+}
