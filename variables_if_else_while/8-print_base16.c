569efee429b86805b10d794872e89d2b8f220fcd
Ian <Gibbu_@hotmail.com>
2025-02-17 14:18:25 +1100

    stuff

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Main
+ *
+ * Return: return
+ */
+int main(void)
+{
+	int i;
+	int hex;
+
+	for (i = 0; i < 16; i++)
+	{	
+		hex = i < 10 ? '0' : 'a' - 10;
+		putchar(hex);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
