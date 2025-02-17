24f1e0979616342b37d1cb6369f39104af575f0b
Ian <Gibbu_@hotmail.com>
2025-02-17 13:45:20 +1100

    stuff

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 'a'; i <= 'z'; i++)
-                putchar(i);
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
