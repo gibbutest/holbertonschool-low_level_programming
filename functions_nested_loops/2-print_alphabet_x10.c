8be9ec00884fa23c5c7637c99dc8c5792597169a
Ian <Gibbu_@hotmail.com>
2025-02-18 13:44:34 +1100

    stuff

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,18 @@
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int c;
-  int i;
+	int i;
 
-  for (i = 0; i < 10; i++)
-    for (c = 'a'; c <= 'z'; c++)
-      _putchar(c);
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+	}
 
-	_putchar('\n');
 }
