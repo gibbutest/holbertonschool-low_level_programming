2d0ad7e4d6f5b38c20d6e30963b6ec7f51cdb5c4
Ian <Gibbu_@hotmail.com>
2025-02-18 14:02:23 +1100

    stuff

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - Is it lower? Like... Actually?
+ * @c: The char to be checked
+ *
+ * Return: yes or no
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'Z');
+}
