13ac48dd1ad72237b991a07ddfe4ef028dd8b37f
Ian <Gibbu_@hotmail.com>
2025-02-18 13:28:49 +1100

    stuff

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Main
+ *
+ * Return: return
+ */
+int main(void)
+{
+	char *string = "abcdefgihjklmnopqrstuvwxyz\n";
+
+	while (*string)
+		_putchar(*string++);
+
+	return (0);
+}
