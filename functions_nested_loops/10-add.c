fd2f9783a512a05a36e1a55ba04ca804119afbf8
Ian <Gibbu_@hotmail.com>
2025-02-21 15:01:31 +1100

    stuff

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add the numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result of the two
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
