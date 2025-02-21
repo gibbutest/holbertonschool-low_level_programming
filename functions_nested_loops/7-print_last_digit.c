4492c67fbda804b27b311a4f088aafa701347b6f
Ian <Gibbu_@hotmail.com>
2025-02-21 13:43:01 +1100

    stuffs

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - The printing of the last digit.
+ * @n: The number
+ *
+ * Return: beans
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	_putchar('0' + last);
+
+	return (last);
+}
