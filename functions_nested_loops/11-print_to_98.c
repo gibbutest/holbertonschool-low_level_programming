6be0da5392a74723bc8d700521f97ebb0dba7a20
Ian <Gibbu_@hotmail.com>
2025-02-21 15:18:26 +1100

    stuff

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,17 +3,15 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n > 98)
 	{
-		printf("%d\n", n);
-	}
-	else if (n > 98)
-	{
-		while (n > 98)
+		while (n >= 98)
 			printf("%d, ", n--);
 	}
 	else {
-		while (n < 98)
+		while (n <= 98)
 			printf("%d, ", n++);
 	}
+
+	printf("98\n");
 }
