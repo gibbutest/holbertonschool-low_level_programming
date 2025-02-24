653b491037342f9c91799939f45e48bfbc091bd6
Ian <Gibbu_@hotmail.com>
2025-02-24 15:46:14 +1100

    stuff

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,18 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			if (i % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else
-			{
-				printf("Fizz");
-			}
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
@@ -32,7 +29,7 @@ int main(void)
 			printf("%d", i);
 		}
 
-		if (i == 99)
+		if (i == 100)
 		{
 			putchar('\n');
 		}
