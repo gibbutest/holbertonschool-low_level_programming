d2718c445a6e960b84de512f47228a17f7801a27
Ian <Gibbu_@hotmail.com>
2025-02-24 15:19:17 +1100

    stuff

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - What's the weather like?
+ *
+ * Description: It's raining sideways!
+ * @n: But how much rain?
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < n; b++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
