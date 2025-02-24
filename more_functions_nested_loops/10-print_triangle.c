adfbd6603221580a8016497b3eebd59c6816672e
Ian <Gibbu_@hotmail.com>
2025-02-24 16:10:42 +1100

    stuff

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Illuminati?
+ *
+ * Description: Not quite right.
+ * @size: How big is the illuminati?
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	for (a = size; a != 0; a--)
+	{
+		for (b = 0; b < size; b++)
+		{
+			if (b > a)
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
