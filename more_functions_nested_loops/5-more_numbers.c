c4ed5d318344f5428d8b31202bb10efc6266f70f
Ian <Gibbu_@hotmail.com>
2025-02-24 15:08:45 +1100

    stuff

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ void more_numbers(void)
 {
 	int a;
 	int b;
-	
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
 				_putchar('0' + (b / 10));
-			
+
 			_putchar('0' + (b % 10));
 		}
 
