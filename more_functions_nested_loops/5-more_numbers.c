a84e345df468712f5074390bdd0cfe6fcfe7806c
Ian <Gibbu_@hotmail.com>
2025-02-24 15:02:47 +1100

    stuff

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - MOAR!
+ *
+ * Description: MOOOOAAAR!!!!
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+	
+	for (a = 0; a < 11; a++)
+		for (b = 0; b < 15; b++)
+			_putchar('0' + b);
+
+	_putchar('\n');
+}
