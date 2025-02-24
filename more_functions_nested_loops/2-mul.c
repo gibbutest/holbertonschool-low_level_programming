aacb8335f3a9816ae6b27b2283c31db5ea39aa63
Ian <Gibbu_@hotmail.com>
2025-02-24 14:50:50 +1100

    stuff

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - How is this a week 4 question?
+ *
+ * Description: a * b = ?
+ * @a: First
+ * @b: Second
+ *
+ * Return: a * b = ?
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
