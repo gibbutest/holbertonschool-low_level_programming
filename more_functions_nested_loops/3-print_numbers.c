15199844ea388b4b9cefb6223e48d5707c3c25d5
Ian <Gibbu_@hotmail.com>
2025-02-24 14:54:32 +1100

    stuff

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print all the numbers
+ *
+ * Description: Why do we need a separate description if the stuff after the - is meant to be just that?
+ */ 
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar('0' + i);
+
+	_putchar('\n');
+}
