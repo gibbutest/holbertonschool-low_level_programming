502304d5493acfe83244036acf71023803f163e0
Ian <Gibbu_@hotmail.com>
2025-02-25 16:10:09 +1100

    stuff

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; n > 0; i++)
+	for (i = 0; n > 1; i++)
 	{
 		printf("%d, ", a[i]);
 		n--;
 	}
+	if (n == 1)
+		printf("%d", a[i]);
 
 	_putchar('\n');
 }
