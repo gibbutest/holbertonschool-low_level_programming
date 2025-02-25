913a0a97f9f4947dd0201c4387fe48118f7dfe16
Ian <Gibbu_@hotmail.com>
2025-02-25 13:32:15 +1100

    stuff

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * _strlen - Length of string
+ *
+ * Description: yes
+ * @s: the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (char[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
