1b451916bae135b960d27cdb70874ce6ab5b5624
Ian <Gibbu_@hotmail.com>
2025-02-25 15:11:26 +1100

    stuff

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -20,7 +20,7 @@ void puts_half(char *str)
 	if (length % 2 == 0)
 		half = length / 2;
 	else
-		half = (length - 1) / 2;
+		half = (length + 1) / 2;
 
 	for (i = half; i < length; i++)
 		_putchar(str[i]);
