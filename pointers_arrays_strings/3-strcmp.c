3ec293ef526ac2e9e8b8d7c5b4646e1d88c14811
Ian <Gibbu_@hotmail.com>
2025-03-01 17:31:20 +1100

    stuff

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - eYes
+ *
+ * Description: Just does the things, innit?
+ * @s1: First
+ * @s2: Second
+ *
+ * Return: Is equal?
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 > *s2) - (*s1 < *s2);
+}
