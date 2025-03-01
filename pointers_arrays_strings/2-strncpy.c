cc58553933aa31f3bdab32f86598b1d5a1319753
Ian <Gibbu_@hotmail.com>
2025-03-01 17:19:28 +1100

    stuff

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,16 @@
 #include "main.h"
 
-/**
- * _strncpy - Replace is a better word for it.
- *
- * Description: Overwrites the original with the new.
- * @dest: The original
- * @src: The new
- * @n: Limit
- *
- * Return: The new charset.
- */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 	char *tmp = dest;
 
-	for (; dest[i] != '\0' && i <= n; i++)
+	for (; dest[i] && i < n; i++)
 	{
-		tmp[i] = src[i];
+		if (src[i] != '\0')
+			tmp[i] = src[i];
+		else
+			dest[i] = '\0';
 	}
 
 	return (tmp);
