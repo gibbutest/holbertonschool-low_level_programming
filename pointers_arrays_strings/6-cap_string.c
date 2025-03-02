ce26050eba2abe59497424c0114df954e81a18bc
Ian <Gibbu_@hotmail.com>
2025-03-02 16:34:43 +1100

    stuff

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,6 +15,9 @@ char *cap_string(char *s)
 	int b;
 	char sep[] = " \t\n,;.!?\"(){}";
 
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+
 	for (a = 0; s[a]; a++)
 		for (b = 0; sep[b]; b++)
 			if (s[a] == sep[b] && (s[a + 1] >= 'a' && s[a + 1] <= 'z'))
