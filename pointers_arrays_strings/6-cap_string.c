75cac422192223244b6de825ee63bdedfe96a131
Ian <Gibbu_@hotmail.com>
2025-03-02 16:33:04 +1100

    stuff

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,24 +11,14 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0;
+	int a;
+	int b;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] = s[0] - 32;
-
-	for (; s[i]; i++)
-	{
-		if (s[i] < 65 || s[i] > 122)
-		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				if (s[i + 1] != '6' && s[i + 1] != '-')
-				{
-					s[i + 1] = s[i + 1] - 32;
-				}
-			}
-		}
-	}
+	for (a = 0; s[a]; a++)
+		for (b = 0; sep[b]; b++)
+			if (s[a] == sep[b] && (s[a + 1] >= 'a' && s[a + 1] <= 'z'))
+				s[a + 1] = s[a + 1] - 32;
 
 	return (s);
 }
