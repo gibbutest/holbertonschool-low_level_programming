d580d4c235e1a69ae81e2ed8faa74c38c6538bf9
Ian <Gibbu_@hotmail.com>
2025-03-02 15:34:37 +1100

    stuff

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,9 +17,13 @@ char *cap_string(char *s)
 
 	for (; s[i]; i++)
 		if (s[i] < 65 || s[i] > 122)
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				if (s[i + 1] != '6' && s[i] != '-')
-					s[i + 1] = s[i + 1] - 32;
+		{
+			int next = s[i + 1];
+
+			if (next >= 'a' && next <= 'z')
+				if (next != '6' && next != '-')
+					next = next - 32;
+		}
 
 	return (s);
 }
