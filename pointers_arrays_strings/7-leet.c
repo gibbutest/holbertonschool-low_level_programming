a66c20b7c95a6fb3af67a50ebc96cf9b7bbf05b2
Ian <Gibbu_@hotmail.com>
2025-03-02 16:50:26 +1100

    stuff

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * leet - l337
+ *
+ * Description: 7r4N5F0rM 70 1337 5P34K
+ * @s: 7H3 57r1N6 70 C0NV3r7
+ *
+ * Return: 7H3 1337 5P34K
+ */
 char *leet(char *s)
 {
 	int a;
