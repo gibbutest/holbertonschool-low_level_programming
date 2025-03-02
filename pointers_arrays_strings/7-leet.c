d4da31cbc8e772836c15f5b6a3a7cf2158d0ccea
Ian <Gibbu_@hotmail.com>
2025-03-02 16:48:33 +1100

    stuff

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+	int a;
+	int b;
+	char norms[10] = {"aAeEoOtTlL"};
+	char leet[10] = {"44330077lL"};
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; leet[b]; b++)
+			if (s[a] == norms[b])
+				s[a] = leet[b];
+	}
+
+	return (s);
+}
