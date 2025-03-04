6c4a3bc158dde9d8dc16823c446bb5957d5531b5
Ian <Gibbu_@hotmail.com>
2025-03-04 14:09:38 +1100

    stuff

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - An actual useful function?
+ *
+ * Description: That's crazy.
+ * @haystack: The string to search.
+ * @needle: The string to search in the haystack.
+ *
+ * Return: Pointer or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *tmp = haystack;
+	
+		for (; *needle != '\0' && *tmp == *needle; needle++)
+			tmp++;
+
+		if (*needle == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
