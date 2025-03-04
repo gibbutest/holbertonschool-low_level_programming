3d57437ed925e0126e8d0f65e4a7c31c52c19e2a
Ian <Gibbu_@hotmail.com>
2025-03-04 14:31:58 +1100

    stuff

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - Print the stuffs
+ * @a: The array
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	for (i = 0; i < size * size; i = i + size + 1)
+	{
+		x = a[i];
+		y = a[(size * z) - z];
+		z++;
+	}
+
+	printf("%d, %d\n", z, y);
+}
