7cb258fa634c2d056a565c7d8c92a42a9ba13666
Ian <Gibbu_@hotmail.com>
2025-03-04 15:03:22 +1100

    stuff

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - noisrucer_ver_tnirp
+ * 
+ * Description: Print in reverse????
+ * @s: The string
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
