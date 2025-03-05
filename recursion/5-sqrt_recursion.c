1cf9e035e1ef973e3a9e48e605aa6d0fbc992baf
Ian <Gibbu_@hotmail.com>
2025-03-05 13:12:14 +1100

    stuff

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,28 @@
 #include "main.h"
 
+/**
+ * sqrt - Skr skr
+ *
+ * Description: Loop until found the lowest square root.
+ * @n: The number
+ * @base: The base case
+ * 
+ * Return: Numbers!
+ */
+int get_square(int n, int base)
+{
+	return base * base > n ? (-1) : base * base == n ? base : get_square(n, base + 1);
+}
+
+/**
+ * _sqrt_recursion - We looping bois
+ *
+ * Description: Loop until found the lowest square root.
+ * @n: The number
+ *
+ * Return: Numbers!
+ */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n <= 1)
-		return (n);
-
-	n = _sqrt_recursion(n - 1) + n + n - 1;
-	return (n);
+	return n < 0 ? (-1) : get_square(n, 1);
 }
