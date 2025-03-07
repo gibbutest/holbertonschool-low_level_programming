b2a87854a008965a0ba640647d3670255593633a
Ian <Gibbu_@hotmail.com>
2025-03-07 11:53:57 +1100

    stuff

---
+++ argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i] == '\0')
+			break;
+
+		if (argv[i] >= '0' && argv[i] <= '9')
+			sum += atoi(argv[i]);
+		else {
+			printf("Error\n");
+			return (0);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
