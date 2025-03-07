5113762ab7e01a2793776eccdf4c24883a7b0a03
Ian <Gibbu_@hotmail.com>
2025-03-07 11:13:27 +1100

    stuff

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Run the code Kronk!
+ * @argc: The arg count
+ * @argv: all the args
+ *
+ * Return: 0
+ */
+int main(int argc, int *argv[])
+{
+	if (argc > 0)
+		print("%s\n", argv[0])
+
+	return (0);
+}
