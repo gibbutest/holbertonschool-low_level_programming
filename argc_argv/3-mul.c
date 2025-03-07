28c244f600cb190a7e2ef0df60369683e27f9b99
Ian <Gibbu_@hotmail.com>
2025-03-07 11:30:18 +1100

    stuff

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Run the code
+ * @argc: The arg count
+ * @argv: The args
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
