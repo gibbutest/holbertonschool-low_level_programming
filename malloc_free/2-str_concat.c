c078e67859c1c7273921d75a12bc2ecfe47ae068
Ian <Gibbu_@hotmail.com>
2025-03-11 12:32:09 +1100

    stuff

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int sum = 0;
+	int pos = 0;
+	char *combined;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		sum++;
+	for (i = 0; s2[i] != '\0'; i++)
+		sum++;
+
+	combined = malloc((sum + 1) * sizeof(char));
+	if (combined == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		combined[pos] = s1[i];
+		pos++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		combined[pos] = s2[i];
+		pos++;
+	}
+	combined[sum] = '\0';
+
+	return (combined);
+}
