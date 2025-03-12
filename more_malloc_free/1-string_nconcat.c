1721e610670706ccbe3597e41d20b9e4cd2ed2dd
Ian <Gibbu_@hotmail.com>
2025-03-12 13:41:26 +1100

    stuff

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Concat string, with a limit on the 2nd string.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The limit of chars for the 2nd string.
+ *
+ * Return: The newly formed string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	int sum = 0;
+	int pos = 0;
+	char *combined;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		sum++;
+	for (i = 0; s2[i] != '\0' && i < n; i++)
+		sum++;
+
+	combined = malloc((sum + 1) * sizeof(char));
+	if (combined == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		combined[pos] = s1[i];
+		pos++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		combined[pos] = s2[i];
+		pos++;
+	}
+	combined[sum] = '\0';
+
+	return (combined);
+}
