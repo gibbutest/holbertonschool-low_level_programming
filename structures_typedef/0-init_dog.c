6e965fcfbb34b0414951c92c6f0f3bdc57c24d05
Ian <Gibbu_@hotmail.com>
2025-03-17 10:55:59 +1100

    stuff

---
+++ structures_typedef/0-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * init_dog - Init the dog!
+ * @d: The dog struct
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
