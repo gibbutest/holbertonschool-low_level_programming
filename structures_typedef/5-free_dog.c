bc9532e26e1ffa0d69ce6aa903db498e3bc286e7
Ian <Gibbu_@hotmail.com>
2025-03-17 11:22:41 +1100

    stuff

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.lib>
+#include "dog.h"
+
+/**
+ * free_dog - BE FREE!!!
+ * @d: The doc struct.
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return (NULL);
+	if (d->owner)
+		free(d->owner);
+	if (d->name)
+		free(d->name);
+	free(d);
+}
