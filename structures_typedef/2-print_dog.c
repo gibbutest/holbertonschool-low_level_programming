e47e9349f1fc085a074bd99b679b64ed2efd431a
Ian <Gibbu_@hotmail.com>
2025-03-17 11:05:58 +1100

    stuff

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,9 +6,14 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 
-	printf("Name: %d\n", d->name || "(nil)");
-	printf("Age: %d\n", d->age || "(nil)");
-	printf("Owner: %d\n", d->owner || "(nil)");
+	if (!d->name)
+		d->name = "(nil)";
+	if (!d->owner)
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 	
 /*
 	if (!d->name)
