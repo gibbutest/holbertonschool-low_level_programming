8d687b7a2a96c7e5a07eecb0a804d5ce764947dd
Ian <Gibbu_@hotmail.com>
2025-03-17 11:06:26 +1100

    stuff

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * print_dog - Prints the dogs details
+ * @d: The dog struct.
+ */
 void print_dog(struct dog *d)
 {
 	if (!d)
@@ -14,13 +18,4 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
-	
-/*
-	if (!d->name)
-		printf("Name: (nil)\n");
-	else
-		printf("Name %s\n", d->name);
-	
-	if (!d->)*/
-
 }
