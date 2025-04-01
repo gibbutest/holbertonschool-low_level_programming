448a6960c96a7b296b975ddefa1032bbc228d193
Ian <Gibbu_@hotmail.com>
2025-04-01 15:48:46 +1100

    stuff

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end
+ * @head: The head/entry ponter
+ * @str: The string to duplicate
+ *
+ * Return: A pointer to the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	list_t *current;
+	int i = 0;
+
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	
+	while(str[i] != '\0')
+		i++;
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	current = *head;
+
+	while (current->next)
+		current = current->next;
+
+	current->next = new;
+
+	return (new);
+}
