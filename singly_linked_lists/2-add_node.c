e3ba712ec6169c75d866cebedcb32e0bfeeac40d
Ian <Gibbu_@hotmail.com>
2025-04-01 15:35:37 +1100

    stuff

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - Add the nodes and stuffs
+ * @head: The head/entry pointer
+ * @str: The string to be duplicated
+ *
+ * Return: Pointer to the new head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	int i;
+
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++);
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
