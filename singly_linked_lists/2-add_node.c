5a9dce509d9d5bec830cfd87e93769ce562c6d08
Ian <Gibbu_@hotmail.com>
2025-04-01 15:37:06 +1100

    stuff

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,7 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
 	new->str = strdup(str);
 	new->len = i;
