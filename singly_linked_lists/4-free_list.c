0d7d7d124d1399634f4fd60b38dfa3b2bc63a009
Ian <Gibbu_@hotmail.com>
2025-04-01 15:54:29 +1100

    stuff

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees the list of all it's problems
+ * @head: The head/entry pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while(head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
