b8ae647098b718b52688254f9dd0ee4559f0784d
Ian <Gibbu_@hotmail.com>
2025-04-03 12:32:03 +1100

    stuff

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,11 +3,11 @@
 /*
  * add_dnodeint - Function stuffs
  *
- * Description: Adds a new node to the end
+ * Description: Adds a new node to the start
  * @head: The head/entry pointer
  * @n: The number to add
  *
- * Return: Pointer to the node node.
+ * Return: Address to the node.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
