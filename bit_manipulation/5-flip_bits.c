8592b760837baa7c8543f7adf6e02716971cedaf
Ian <Gibbu_@hotmail.com>
2025-04-04 14:19:56 +1100

    stuff

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * flip_bits - Flip the bits Kronk!
+ * @n: The number to flip
+ * @m: The number to flip to
+ *
+ * Return: numbers
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int prev = n ^ m;
