d9ae2f444682a39fca2cf4f704042cb97c2f8103
Ian <Gibbu_@hotmail.com>
2025-04-04 13:18:00 +1100

    stuff

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 	unsigned int i;
 
-	if (!*head || index < 0)
+	if (!*head)
 		return (-1);
 
 	for (i = 0; tmp != NULL && i < index; i++)
