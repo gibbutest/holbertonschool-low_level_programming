0484f81a75bb92cfefd1ee1417f349554a455684
Ian <Gibbu_@hotmail.com>
2025-04-04 12:07:36 +1100

    stuff

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees the memory for th entire list
+ * @head: The head/entry pointer
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head)
+	{
+		if (head->next)
+			free(head->next);
+		if (!head->next)
+		{
+			free(head);
+			return;
+		}
+
+		head = head->next;
+	}
+}
