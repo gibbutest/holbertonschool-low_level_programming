003c7fc3ac6e03684e89f50cb48f6355cd5b66b4
Ian <Gibbu_@hotmail.com>
2025-04-07 14:35:52 +1000

    stuff

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with text
+ * @filename: The name of the file
+ * @text_content: The text add to the file
+ *
+ * Return: Success or error code.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int writeVal;
+	int i = 0;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_APPEND | O_WRONLY);
+
+	if (!text_content)
+		return (1);
+
+	while (text_content[i])
+		i++;
+
+	writeVal = write(file, text_content, i);
+	if (file == -1 || writeVal == -1)
+		return (-1);
+
+	return (1);
+}
